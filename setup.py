803a73237b37c6d37d6625596582a1d16bcfb591
Edmund Jorgensen <tomheon@gmail.com>
2012-05-22 22:09:00 -0400

    Bump version in setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ from distutils.dir_util import remove_tree, copy_tree, mkpath
 
 if __name__=='__main__':
     setup(name="ldaptor",
+          version="0.0.1",
           description="Pure-Python library for LDAP",
           long_description="""
 
